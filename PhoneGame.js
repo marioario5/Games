68a6d79dd699c342310fc67b816e67819724d947
Mario <Javier.Perez@calix.com>
2018-08-09 18:07:48 -0700

    darth yoda improved

--- PhoneGame.js
+++ PhoneGame.js
@@ -1,60 +1,54 @@
 /*
-Hi! I will give you instructions for how to play this game. First, you click restart and then IMPOTANT!!!!! click the screen where you play to give focus to the screen. then, try to collect the cristals. Change the number of cristals that fall with the variable cristalNumber. Coming soon: if you collect the red cristals, I have a darth function that will make yoda DARTH YODA!
+Hi! I will give you instructions for how to play this game. First, you click restart and then IMPORTANT!!!!! click the screen where you play to give focus to the screen. then, try to collect the cristals. Change the number of cristals that fall with the variable cristalNumber. Coming soon: if you collect the red cristals, I have(not will have) a darth function that will make yoda DARTH YODA!
 */
 //start
 var colors=[color(0, 255, 251),color(17, 255, 0),color(251, 255, 0),color(157, 0, 255),color(255, 0, 0)];
+
 var cristalNumber=2; //Change for number of cristals
 var Cristals = [];//array
 //to make the yoda class
 var Yoda = function(){
     this.x=200;
     this.y=303;
+    this.eyecolor=color(0, 0, 0);
+    this.HeadEarsFeetcolor=color(2,179,2);
+    this.tuniccolor=color(148, 105, 49);
+    this.isDarth=false;
 };
-
+Yoda.prototype.colorDarth=function(){
+    this.eyecolor=color(255, 234, 0);
+    this.HeadEarsFeetcolor=color(255, 0, 0);
+    this.tuniccolor=color(0, 0, 0);
+};
+Yoda.prototype.drawEyebrows=function(){//eyebrow method
+    fill(0, 0, 0);
+    strokeWeight(2);
+    line(this.x+5,this.y+3,this.x+20,this.y+11);
+    line(this.x+43,this.y+3,this.x+28,this.y+11);    
+};
 //how to draw yoda
-Yoda.prototype.draw  =function(darth){
-    if(! darth){//head
-        fill(2, 179, 2);
-    }else{
-        fill(255, 0, 0);
-    }
+Yoda.prototype.draw  =function(){
+    //head
+    fill(this.HeadEarsFeetcolor);
     rect(this.x,this.y,50,35);
-    if(! darth){//eyes
-        fill(0, 0, 0);
-    }else{
-        fill(255, 234, 0);
-    }
+    fill(this.eyecolor);
     ellipse(this.x+13,this.y+13,8,8);
     ellipse(this.x+36,this.y+13,8,8);
     line(this.x+10,this.y+25,this.x+39,this.y+25);
-    if(! darth){//ears
-        fill(2, 179, 2);
-    }else{
-        fill(255, 0, 0);    
-    }
+    //ears
+    fill(this.HeadEarsFeetcolor);
     triangle(this.x+-32,this.y+-12,this.x,this.y,this.x+-1,this.y+19);
     triangle(this.x+86,this.y-12,this.x+50,this.y,this.x+50,this.y+19);
-    if(! darth){//tunic
-        fill(148, 105, 49);
-    }else{
-        fill(0, 0, 0);
-    }
+    //tunic
+    fill(this.tuniccolor);
     rect(this.x,this.y+35,50,35);
     triangle(this.x+0,this.y+34,this.x+-21,this.y+71,this.x+0,this.y+70);
     triangle(this.x+50,this.y+35,this.x+49,this.y+70,this.x+74,this.y+71);
-    if(! darth){//feet
-        fill(2, 179, 2);
-    }else{
-        fill(255, 0, 0);
-    }
+    //feet
+    fill(this.HeadEarsFeetcolor);
     rect(this.x,this.y+70,19,5);
     rect(this.x+31,this.y+70,19,5);
-    if(darth){//eye brows
-        fill(0, 0, 0);
-        strokeWeight(2);
-        line(this.x+5,this.y+3,this.x+20,this.y+11);
-        line(this.x+43,this.y+3,this.x+28,this.y+11);
-    }
+    //eyebrows
     strokeWeight(1);
     fill(17, 84, 0);//ground
     rect(0,378,400,98);
@@ -111,6 +105,10 @@ var draw = function() {
         Cristals[y].isColliding(y);
     }
     yoda.draw();
+    if(yoda.isDarth){
+        yoda.drawEyebrows(); 
+        yoda.colorDarth();
+    }
     fill(255, 0, 0);
     //make him move
     if (keyIsPressed && keyCode === RIGHT) {
