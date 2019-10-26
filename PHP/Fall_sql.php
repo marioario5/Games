efd0376360b7f1ead32b41f1ecf360f3b66165f9
Mario Perez <marioario@oiram.attlocal.net>
2019-10-25 20:08:00 -0700

    Dropdown with bug

---
+++ PHP/Fall_sql.php
@@ -0,0 +1,27 @@
+ <?php
+$Name=$_REQUEST['name']; 
+$Plate=$_REQUEST['plate'];
+$Why=$_REQUEST['why'];
+$servername = "127.0.0.1";
+$username = "marioario";
+$password = "!Banana0o";
+$dbname = "Fall_Form";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Fall_Form (Name,Plate,Why)
+VALUES ('$Name', '$Plate', '$Why')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Thank you for filling out this form! Your plate is: ". $Plate;
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
